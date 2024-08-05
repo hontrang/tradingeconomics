facb8c0e49cc166f4f3f8f963c1b088a7fe3bb04
hon.trang <hon.trang@isobar.com>
2024-08-05 16:27:18 +0700

    fix: chỉ lấy dữ liệu US

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { writeFile } from 'fs';
 
 test('test', async ({ page }) => {
   await page.goto('https://tradingeconomics.com/calendar');
+  await page.getByRole('button', { name: '  Countries' }).click();
+  await page.getByText('Clear').click();
+  await page.locator('#te-c-all li').filter({ hasText: 'United States' }).click();
+  await page.getByRole('cell', { name: '  Recent    Impact  ' }).click();
+  await page.getByRole('button', { name: '   Category' }).click();
+  await page.getByText('Save').click();
   const content = await page.content();
   writeFile('./docs/index.html', content, (err) => {
     if (err) {
